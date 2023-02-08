5f69d0d03d61a72aa9d25141e3cfad55f4efb20e
gilingan2 <99949737+gilingan2@users.noreply.github.com>
2023-02-08 13:27:12 +0700

    update cors config in index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import postRoutes from './routes/postRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+  }));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/dalle', dalleRoutes);
